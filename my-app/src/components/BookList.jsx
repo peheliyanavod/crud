92b3950200ea301e802607eb5946ebcc9af398b9
peheliyanavod <hwpeheliya@gmail.com>
2024-06-08 20:20:52 +0530

    Create the table structure in BookList.jsx

---
+++ my-app/src/components/BookList.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function BookList() {
+  return (
+    <div>
+        <h2>Books Information System</h2>
+        <table>
+            <tr>
+                <th>Book id</th>
+                <th>Name</th>
+                <th>Auther</th>
+                <th>Price</th>
+                <th>Action</th>
+            </tr>
+            <tr>
+                <td>Book id</td>
+                <td>Name</td>
+                <td>Auther</td>
+                <td>Price</td>
+                <td>Action</td>
+            </tr>
+        </table>
+    </div>
+  )
+}
